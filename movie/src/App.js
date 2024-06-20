c91737f2540bed292e987b104b85313bef0003aa
priyanshu Maurya <sparkpriyanshu400@gmail.com>
2024-06-20 17:47:35 +0530

    my first react commit

---
+++ movie/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellow world</h1>
+      <h1>Hellow wod{2+2}</h1>
+    </div>
+      
+      
+  );
+}
+
+export default App;
